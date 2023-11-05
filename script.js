967e092d3d54135a2640524fc52efcfd28e134d8
gabrielli-lima <gabriellilima165@gmail.com>
2023-11-04 23:22:57 -0300

    bug fix in menu

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-var menu = document.getElementsByClassName("menu")[0];
-var iconMenu = document.getElementById("iconMenu");
+let isOpen = false;
+let menuContainer = document.querySelector(".menuContainer");
+let iconMenu = document.getElementById("iconMenu");
 
-iconMenu.addEventListener("click", function () {
+iconMenu.addEventListener("click", modalMenu);
 
-    menu.style.visibility = "visible";
-
-    menu.style.animation = "menu .5s cubic-bezier(.25,.46,.45,.94) both"
-}) 
+function modalMenu() {
+    if (!isOpen) {
+        menuContainer.style.visibility = "visible";
+        menuContainer.style.animation = "menu-open .5s cubic-bezier(.25,.46,.45,.94) both";
+        isOpen = true;
+    } else {
+        menuContainer.style.visibility = "hidden";
+        menuContainer.style.animation = "menu-close .5s cubic-bezier(.25,.46,.45,.94) both";
+        isOpen = false;
+    }
+}
